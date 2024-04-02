955925d7036f3c2ec8d70f82ff81c5ce80c90098
Keithjmm <joenormal79@gmail.com>
2024-04-02 15:38:09 +0000

    reconfigure index.js and app.js to remove router conflict

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <div>
+        <div>s
           <Navbar />
           <Routes>
   <Route path="/" element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -8,11 +8,5 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
-  </React.StrictMode>
+  <App />  
 );
